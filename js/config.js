12694534ecf9f2b83a7587e716820b065ed6ec1a
zjgtree <77392965+zjgtree@users.noreply.github.com>
2025-09-03 14:27:13 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -43,6 +43,73 @@ name: '暴风资源'
 tyyszy: {
 api: 'https://tyyszy.com/api.php/provide/vod',
 name: '天涯资源'
+},
+    ffzy: {
+api: 'http://ffzy5.tv/api.php/provide/vod',
+name: '非凡影视',
+detail: 'http://ffzy5.tv'
+},
+heimuer: {
+api: 'https://json.heimuer.xyz/api.php/provide/vod',
+name: '黑木耳',
+detail: 'https://heimuer.tv'
+},
+zy360: {
+api: 'https://360zy.com/api.php/provide/vod',
+name: '360资源'
+},
+iqiyi: {
+api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
+name: 'iqiyi资源'
+},
+wolong: {
+api: 'https://wolongzyw.com/api.php/provide/vod',
+name: '卧龙资源'
+},
+hwba: {
+api: 'https://cjhwba.com/api.php/provide/vod',
+name: '华为吧资源'
+},
+jisu: {
+api: 'https://jszyapi.com/api.php/provide/vod',
+name: '极速资源',
+detail: 'https://jszyapi.com'
+},
+dbzy: {
+api: 'https://dbzy.com/api.php/provide/vod',
+name: '豆瓣资源'
+},
+mozhua: {
+api: 'https://mozhuazy.com/api.php/provide/vod',
+name: '魔爪资源'
+},
+mdzy: {
+api: 'https://www.mdzyapi.com/api.php/provide/vod',
+name: '魔都资源'
+},
+zuid: {
+api: 'https://api.zuidapi.com/api.php/provide/vod',
+name: '最大资源'
+},
+yinghua: {
+api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
+name: '樱花资源'
+},
+baidu: {
+api: 'https://api.apibdzy.com/api.php/provide/vod',
+name: '百度云资源'
+},
+wujin: {
+api: 'https://api.wujinapi.me/api.php/provide/vod',
+name: '无尽资源'
+},
+wwzy: {
+api: 'https://wwzy.tv/api.php/provide/vod',
+name: '旺旺短剧'
+},
+ikun: {
+api: 'https://ikunzyapi.com/api.php/provide/vod',
+name: 'iKun资源'
 }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
