cabd63810307bfb84b2d034d6f6ed30aae970c40
zjgtree <77392965+zjgtree@users.noreply.github.com>
2025-10-27 10:17:27 +0800

    Add multiple resource APIs to config.js

--- js/config.js
+++ js/config.js
@@ -110,6 +110,47 @@ name: '旺旺短剧'
 ikun: {
 api: 'https://ikunzyapi.com/api.php/provide/vod',
 name: 'iKun资源'
+},
+ckzy: {
+api: 'https://www.ckzy1.com/api.php/provide/vod',
+name: 'CK资源',
+adult: true
+},
+jkun: {
+api: 'https://jkunzyapi.com/api.php/provide/vod',
+name: 'jkun资源',
+adult: true
+},
+bwzy: {
+api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+name: '百万资源',
+adult: true
+},
+souav: {
+api: 'https://api.souavzy.vip/api.php/provide/vod',
+name: 'souav资源',
+adult: true
+},
+r155: {
+api: 'https://155api.com/api.php/provide/vod',
+name: '155资源',
+adult: true
+},
+lsb: {
+api: 'https://apilsbzy1.com/api.php/provide/vod',
+name: 'lsb资源',
+adult: true
+},
+huangcang: {
+api: 'https://hsckzy.vip/api.php/provide/vod',
+name: '黄色仓库',
+adult: true,
+detail: 'https://hsckzy.vip'
+},
+yutu: {
+api: 'https://yutuzy10.com/api.php/provide/vod',
+name: '玉兔资源',
+adult: true
 }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
